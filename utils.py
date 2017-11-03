7c372c87222d7cd14af8394fe94480ca3701429a
D.A.Bashkirtsevich <bashkirtsevich@gmail.com>
2017-11-04 00:01:52 +0500

    Refactor utils
    Using to_bytes/from_bytes instead pack/unpack and inexplicible xor implementation

--- utils.py
+++ utils.py
@@ -2,7 +2,6 @@ import os
 import math
 
 from socket import inet_ntoa, inet_aton
-from struct import unpack, pack
 
 
 def generate_id():
@@ -14,12 +13,7 @@ def generate_node_id():
 
 
 def xor(node_one_id, node_two_id):
-    result = 0
-
-    for i in range(len(node_one_id)):
-        result = (result << 8) + (node_one_id[i] ^ node_two_id[i])
-
-    return result
+    return int.from_bytes(node_one_id, "big") ^ int.from_bytes(node_two_id, "big")
 
 
 def get_routing_table_index(distance):
@@ -36,11 +30,8 @@ def decode_nodes(nodes):
     for i in range(0, len(nodes), 26):
         node_id = nodes[i: i + 20]
 
-        try:
-            ip = inet_ntoa(nodes[i + 20: i + 24])  # from network order to IP address
-            port = unpack("!H", nodes[i + 24: i + 26])[0]  # "!" means to read by network order
-        except:
-            continue
+        ip = inet_ntoa(nodes[i + 20: i + 24])  # from network order to IP address
+        port = int.from_bytes(nodes[i + 24: i + 26], "big")
 
         yield node_id, ip, port
 
@@ -48,13 +39,9 @@ def decode_nodes(nodes):
 def encode_nodes(nodes):
     result = bytes()
 
-    for node in nodes:
-        node_id, ip, port = node
-        try:
-            ip_message = inet_aton(ip)
-            port_message = pack("!H", port)
-        except:
-            continue  # from IP address to network order
+    for node_id, ip, port in nodes:
+        ip_message = inet_aton(ip)
+        port_message = port.to_bytes(2, "big")
 
         result = result + node_id + ip_message + port_message
 
